9c5d7d7cc94922a5695f8e94e20d211889588125
Veeramaneni Runvisha <runvishaveeramaneni@gmail.com>
2025-11-25 23:27:37 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/task-manager/',   // change if you deploy to a sub-folder
+  base: '/task-manager/',
+  server: {
+    port: 3000,
+    open: true 
+  }     // change if you deploy to a sub-folder
 })
