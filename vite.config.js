7f37e4bc935edcbbaaa83dc41f38b94f49658ac5
Veeramaneni Runvisha <runvishaveeramaneni@gmail.com>
2025-11-26 00:49:02 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/task-manager/',
+  base: '/frontapp1/',
   server: {
     port: 3000,
     open: true 
