b9c897497cc479430dc21442303895dcbfef418a
Veeramaneni Runvisha <runvishaveeramaneni@gmail.com>
2025-11-26 01:07:25 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   base: '/frontapp1/',
-  server: {
-    port: 3000,
-    open: true 
-  }     // change if you deploy to a sub-folder
+     // change if you deploy to a sub-folder
 })
